436ca3a59ed1093c8fe9da264bf95809d255b9da
Saar Ibuki <ibukisaar@hotmail.com>
2016-10-30 23:56:33 +0800

    添加COM接口

---
+++ Coolest/SoundOut/Windows/ComInterface/IAudioClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coolest.SoundOut.Windows.ComInterface {
+	[ComImport]
+	[Guid("1CB9AD4C-DBFA-4c32-B178-C2F568A703B2")]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[SuppressUnmanagedCodeSecurity]
+	unsafe interface IAudioClient : IUnknown {
+		int Initialize(ShareMode shareMode, AudioClientStreamFlags streamFlags, long hnsBufferDuration, long hnsPeriodicity, WaveFormat format, void* audioSessionGuid = null);
+		int GetBufferSize([Out] out int bufferFrames);
+		int GetStreamLatency([Out] out long phnsLatency);
+		int GetCurrentPadding([Out] out int paddingFrames);
+		int IsFormatSupported(ShareMode shareMode, WaveFormat format, IntPtr closedMatch);
+		int GetMixFormat([Out] out WaveFormat deviceFormat);
+		int GetDevicePeriod([Out] out long phnsDefaultDevicePeriod, [Out] out long phnsMinimumDevicePeriod);
+		int Start();
+		int Stop();
+		int Reset();
+		int SetEventHandle(IntPtr eventHandle);
+		int GetService(ref Guid guid, [Out] out IntPtr ppv);
+	}
+}
---
+++ Coolest/SoundOut/Windows/StorageAccess.cs
@@ -0,0 +1,21 @@
+﻿namespace Coolest.SoundOut.Windows {
+	/// <summary>
+	/// Specifies how to open a property store.
+	/// </summary>
+	enum StorageAccess {
+		/// <summary>
+		/// Readable only.
+		/// </summary>
+		Read = 0x00000000,
+
+		/// <summary>
+		/// Writeable but not readable.
+		/// </summary>
+		Write = 0x00000001,
+
+		/// <summary>
+		/// Read- and writeable.
+		/// </summary>
+		ReadWrite = 0x00000002
+	}
+}
